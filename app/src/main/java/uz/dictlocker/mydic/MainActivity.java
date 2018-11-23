989e2f609ed21e369b3c2bce01985b94573577e0
Shams <shamspochta@gmail.com>
2018-11-23 13:39:17 +0500

    Commit 2018.11.23 13:29

--- app/src/main/java/uz/dictlocker/mydic/MainActivity.java
+++ app/src/main/java/uz/dictlocker/mydic/MainActivity.java
@@ -12,9 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    MenuItem menuSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +57,10 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        menuSetting = menu.findItem(R.id.action_settings);
         return true;
     }
 
@@ -67,10 +72,16 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_eng_kh) {
+            menuSetting.setIcon(R.drawable.english_khmer_1);
+        }else if (id == R.id.action_kh_eng) {
+            menuSetting.setIcon(R.drawable.khmer_english_1);
+        }else if (id == R.id.action_kh_kh) {
+            menuSetting.setIcon(R.drawable.khmer_khmer_1);
+        }else {
+            Toast.makeText(this, "Unknown action", Toast.LENGTH_SHORT).show();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
